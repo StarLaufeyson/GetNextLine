36d2ac3e8d4796b98e633196c395f3376ee3e349
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2023-12-17 17:37:42 +0100

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 17:34:41 by marvin            #+#    #+#             */
+/*   Updated: 2023/12/17 17:34:41 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	length;
+	size_t	i;
+	char	*duplicate;
+	
+	length = ft_strlen(s);
+	duplicate = (char *)malloc((length + 1) * sizeof(char));
+	if (duplicate != NULL)
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			duplicate[i] = s[i];
+			i++;
+		}
+		duplicate[i] = '\0';
+	}
+	return (duplicate);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	size_t	total_len;
+	char	*result;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	total_len = len1 + len2;
+	result = (char *)malloc((total_len + 1) * sizeof(char));
+	if (result == NULL)size_t	total_len;
+		return (NULL);
+	ft_memcpy(result, s1, len1);
+	ft_memcpy(result + len1, s2, len2 + 1);
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str != (char)c)
+	{
+		if (*str == '\0')
+		{
+			return (NULL);
+		}
+		str++;
+	}
+	return (str);
+}
+
+#include	<stdio.h>
+//copia 'n' bytes de origen/src a destino/dest
+void	*ft_memcpy(void *dest, const void *src, size_t length)
+{
+	char	*d;
+	const char	*s;
+	size_t	i;
+
+	d = dest;
+	s = src;
+	i = 0;
+	while (i < length)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
