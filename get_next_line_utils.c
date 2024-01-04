b4695e8753a074f98256b164472e75f19a99bc7e
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-04 18:15:33 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -58,7 +58,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len2 = ft_strlen(s2);
 	total_len = len1 + len2;
 	result = (char *)malloc((total_len + 1) * sizeof(char));
-	if (result == NULL)size_t	total_len;
+	if (result == NULL)
 		return (NULL);
 	ft_memcpy(result, s1, len1);
 	ft_memcpy(result + len1, s2, len2 + 1);
@@ -81,6 +81,26 @@ char	*ft_strchr(const char *s, int c)
 	return (str);
 }
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	size_t	sub_len;
+	char	*substr;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	sub_len = len;
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (sub_len > (s_len - start))
+		sub_len = s_len - start;
+	substr = (char *)malloc((sub_len + 1) * sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	ft_strlcpy(substr, s + start, sub_len + 1);
+	return (substr);
+}
 //#include	<stdio.h>
 //copia 'n' bytes de origen/src a destino/dest
 void	*ft_memcpy(void *dest, const void *src, size_t length)
