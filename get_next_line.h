6f2a338a700e712cc9eec8d01624a77fb01aae4c
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-04 17:48:14 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -22,7 +22,7 @@
 #  define BUFFER_SIZE 42
 # endif
 
-size_t  ft_strlen(const char *s);
+size_t  ft_strlen(const char *str);
 char    ft_strjoin(char const *s1, char const *s2);
 char    ft_strchr(const char *s, int c);
 char    ft_substr(char const *s, unsigned int start, size_t length);
