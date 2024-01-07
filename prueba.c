e5d996c47b532beefc43a72bc67e8748b1eccf50
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-07 18:00:29 +0100

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -13,8 +13,66 @@
 #include	<fcntl.h>
 #include	<unistd.h>
 #include	<stdio.h>
+/*void	*ft_calloc(size_t num_elements, size_t size)
+{
+	size_t total_size;
+	void *ptr;
+	
+	total_size = num_elements * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, total_size);
+	return (ptr);
+}
 
-int	main(void)
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	unsigned char	*str;
+
+	if (n == 0)
+		return ;
+	i = 0;
+	str = (unsigned char *)s;
+	while (i < n)
+		str[i++] = 0;
+	return ;
+}*/
+#include    "get_next_line.h"
+
+char    *get_next_line(int fd)
+{
+int     bytes_read;
+char    *cup_buffer;    
+
+cup_buffer = ft_calloc (4 + 1, sizeof(char));
+if (cup_buffer == NULL)
+    return (NULL);
+//buffer = malloc (3 + 1 * sizeof(char));
+bytes_read = read(fd, cup_buffer, 3);
+if (bytes_read <= 0)
+    return (NULL);
+return (cup_buffer);
+}
+
+int main(void)
+{
+    int fd;
+    char    *next_line;
+    int  count;
+
+    count = 0;
+    fd = open("ejemplo.txt", O_RDONLY);
+    next_line = get_next_line(fd);
+    count++;
+    printf("[%d]:%s\n", count, next_line); //count is to show you the line numbers
+    next_line = NULL;
+
+    close(fd);
+    return (0);
+}
+/*int	main(void)
 {
 	int		fd;
 	char	buff[70];
@@ -39,4 +97,4 @@ int	main(void)
 		}
 	}
 	return (0);
-}
+}*/
