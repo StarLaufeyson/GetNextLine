f54db0f48b866d43f0a60ab0c8f0a37074e7c7b9
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-08 17:55:11 +0100

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -56,21 +56,25 @@ if (bytes_read <= 0)
 return (cup_buffer);
 }
 
-int main(void)
+int	main(void)
 {
-    int fd;
-    char    *next_line;
-    int  count;
+	int		fd;
+	char	*next_line;
+	int		count;
 
-    count = 0;
-    fd = open("ejemplo.txt", O_RDONLY);
-    next_line = get_next_line(fd);
-    count++;
-    printf("[%d]:%s\n", count, next_line); //count is to show you the line numbers
-    next_line = NULL;
-
-    close(fd);
-    return (0);
+	count = 0;
+	fd = open("archivo.txt", O_RDONLY);
+	while (1)
+	{
+		next_line = get_next_line(fd);
+		if (next_line == NULL)
+			break ;
+		count++;
+		printf("[%d]:%s\n", count, next_line);
+		next_line = NULL;
+	}
+	close(fd);
+	return (0);
 }
 /*int	main(void)
 {
