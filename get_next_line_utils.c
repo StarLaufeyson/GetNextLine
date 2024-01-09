43216c8318d9b797c7e8e0891c346eb244fd0f80
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-09 12:46:48 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -95,6 +95,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr = (char *)malloc((sub_len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
-	ft_strlcpy(substr, s + start, sub_len + 1);
+	ft_strdup(substr, s + start, sub_len + 1);
 	return (substr);
 }
