983a1a620b3833fb9afde96dc434459d03c56da7
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-09 12:17:39 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -23,8 +23,8 @@
 # endif
 
 size_t  ft_strlen(const char *str);
-char    ft_strjoin(char const *s1, char const *s2);
-char    ft_strchr(const char *s, int c);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strchr(const char *s, int c);
 char    ft_substr(char const *s, unsigned int start, size_t length);
 char	  *ft_strdup(const char *s);
 char	  *get_line(char **buffer);
