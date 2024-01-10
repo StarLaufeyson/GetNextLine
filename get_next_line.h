1e9e9c0f27a3539c05015aea2ec0c37fa767a5f8
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-10 18:18:37 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -15,21 +15,22 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-//# include <fcntl.h>
+# include <fcntl.h>
 # include <stddef.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-size_t  ft_strlen(const char *str);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_strchr(const char *s, int c);
-char    *ft_substr(char const *s, unsigned int start, size_t length);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
-char	  *ft_strdup(const char *s);
-char	  *get_line(char **buffer);
-int		  read_line(int fd, char **buffer);
-char    *get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t length);
+char	*ft_strdup(const char *s);
+char	*get_return(char **buffer);
+char	*get_keeper(char **buffer);
+int		read_line(int fd, char **buffer);
+char	*get_next_line(int fd);
 
 #endif
