32ea9ad236e0054d1fb208e14c548fffb4ce67c7
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2024-01-10 18:23:15 +0100

    Update get_next_line_utils_bonus.c

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,68 +10,67 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-////Determina la longitud de una cadena de caracteres
+#include "get_next_line.h"
+
+//Determina la longitud de una cadena de caracteres
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
 	i = 0;
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
-
-////Extrae una subcadena de una cadena previamente dada
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+//Duplica una cadena de caracteres, asignando memoria para una nueva cadena y copiando el nuevo 
+//contenido de la cadena original en la nueva memoria asignada
+char	*ft_strdup(const char *s)
 {
-	size_t	s_len;
-	size_t	sub_len;
-	char	*substr;
+	size_t	length;
+	size_t	i;
+	char	*duplicate;
 
-	if (!s)
+	length = ft_strlen(s);
+	duplicate = (char *)malloc((length + 1) * sizeof(char));
+	if (duplicate == NULL)
 		return (NULL);
-	s_len = ft_strlen(s);
-	sub_len = len;
-	if (start >= s_len)
-		return (ft_strdup(""));
-	if (sub_len > (s_len - start))
-		sub_len = s_len - start;
-	substr = (char *)malloc((sub_len + 1) * sizeof(char));
-	if (substr == NULL)
-		return (NULL);
-	ft_strlcpy(substr, s + start, sub_len + 1);
-	return (substr);
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			duplicate[i] = s[i];
+			i++;
+		}
+		duplicate[i] = '\0';
+	}
+	return (duplicate);
 }
-
 //concatena 2 cadenas s1 y s2 y devuelve una cadena resultante (no requiere conocer tamaño máximo buffer)
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len1;
-	size_t	len2;
-	size_t	total_len;
-	char	*result;
+	char	*ptr;
+	size_t	size;
 
-	if (s1 == NULL || s2 == NULL)
+	size = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc(sizeof(char) * size + 1);
+	if (!ptr)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	total_len = len1 + len2;
-	result = (char *)malloc((total_len + 1) * sizeof(char));
-	if (result == NULL)size_t	total_len;
-		return (NULL);
-	ft_memcpy(result, s1, len1);
-	ft_memcpy(result + len1, s2, len2 + 1);
-	return (result);
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	return (ptr - size);
 }
 
-////Busca última ocurrencia del caracter 'c' en la cadena 's'
+//Busca última ocurrencia del caracter 'c' en la cadena 's'
 char	*ft_strchr(const char *s, int c)
 {
 	char	*str;
 
 	str = (char *)s;
+	if (!s)
+		return (NULL);
 	while (*str != (char)c)
 	{
 		if (*str == '\0')
@@ -83,21 +82,28 @@ char	*ft_strchr(const char *s, int c)
 	return (str);
 }
 
-//#include	<stdio.h>
-//copia 'n' bytes de origen/src a destino/dest
-void	*ft_memcpy(void *dest, const void *src, size_t length)
+//Extrae una subcadena de una cadena previamente dada
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*d;
-	const char	*s;
-	size_t	i;
+	char	*ptr;
+	int		i;
+	size_t	size;
 
-	d = dest;
-	s = src;
+	size = len;
 	i = 0;
-	while (i < length)
+	if (ft_strlen(s) < start || !*s)
 	{
-		d[i] = s[i];
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * (len + 1));
+	ptr[len] = '\0';
+	if (!ptr)
+		return (NULL);
+	while (len-- && s[start])
+	{
+		ptr[i] = s[start];
+		start++;
 		i++;
 	}
-	return (dest);
+	return (ptr);
 }
